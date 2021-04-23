be3f4cb356b281c93cf89ba2db50c54a15654114
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-23 13:08:43 -0400

    commit tree 2e547cca64a0152aa0f72d1e5f399d8d8f2b80bf

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+  get '/welcome/home', to: 'welcome#home'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   # Add your routes here
 end
